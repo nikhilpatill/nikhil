73eb160df81f569d51778f8b0708ddd37a2653bc
nikhilpatil <nikhilpatil1050@gmail.com>
2021-08-05 19:31:04 +0530

    hiii github i want to add kotlin code

---
+++ src/kotlin_code/first.kt
@@ -0,0 +1,16 @@
+package kotlin_code
+
+public open class first {
+
+    fun dis()
+    {
+        println("nikhil ganeesh patil")
+        println(" hello i am start with kotlin and i want to developed andoid application for my carrier")
+    }
+}
+
+
+fun main() {
+    var s=first();
+    s.dis();
+}
